08b1ba3645d41babcf462d76f7ec04c831f951d6
Andrey Mizerov <andrey@mizerov.com>
2025-11-18 21:16:01 +0300

    18

--- app/(authenticated)/systems/actions/index.ts
+++ app/(authenticated)/systems/actions/index.ts
@@ -1,4 +1,3 @@
 export { createSystem, getSystems } from './createSystem'
 export { getSystemById } from './getSystemById'
 export { updateSystem } from './updateSystem'
-export { updateVersion } from '../[id]/versions/actions/updateVersion'
